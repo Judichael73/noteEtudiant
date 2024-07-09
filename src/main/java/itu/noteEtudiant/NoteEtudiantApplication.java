d6e1041466a1d3d1c4c90e8ded7d4c6192acca12
tokyHenintsoa00 <rakotonandrasanajudichael73@gmail.com>
2024-07-09 13:09:55 +0300

    d

---
+++ src/main/java/itu/noteEtudiant/NoteEtudiantApplication.java
@@ -0,0 +1,13 @@
+package itu.noteEtudiant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteEtudiantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteEtudiantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/itu/noteEtudiant/NoteEtudiantApplicationTests.java
@@ -0,0 +1,13 @@
+package itu.noteEtudiant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteEtudiantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
