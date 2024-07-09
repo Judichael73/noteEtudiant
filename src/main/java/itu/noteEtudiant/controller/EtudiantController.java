c4ed28154fe84511fda606347382717cf63152d9
tokyHenintsoa00 <rakotonandrasanajudichael73@gmail.com>
2024-07-09 13:22:37 +0300

    test spring
    test spring

--- src/main/java/itu/noteEtudiant/controller/EtudiantController.java
+++ src/main/java/itu/noteEtudiant/controller/EtudiantController.java
@@ -1,5 +1,15 @@
 package itu.noteEtudiant.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class EtudiantController {
     
+    @RequestMapping("/")
+    public String viewIndex(Model model)
+    {
+        return "home/home";
+    }
 }
